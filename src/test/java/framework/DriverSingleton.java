c80b58d6ce5cc2184c4e08b0e90d407e7138eaf9
Muhammad Ali <muhammad.ali@waqartech.com>
2021-01-26 11:05:24 +0500

    TEST

--- src/test/java/framework/DriverSingleton.java
+++ src/test/java/framework/DriverSingleton.java
@@ -1,66 +1,125 @@
 
+package framework;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.logging.Level;
+import helper.Configutil;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.logging.*;
 
-import java.util.HashMap;
-import java.util.logging.Level;
-
-public class DriverSingleton {
+public class DriverSingleton  {
 
 	public static String seleniumGrid = "false";
 	public static LoggingPreferences logPrefs = new LoggingPreferences();
 	public static WebDriver driver;
 	public static String BrowserX = "Chrome";//"Edge";//"Chrome";
-	//public static String baseurl = "https://www.staging.supermeal.ae";
+	public static String baseurl = "https://staging.supermeal.ae";
 
 
-	public static WebDriver setDriver() {
-		 String os = System.getProperty("os.name").toLowerCase();
-		if (driver == null) {
-			if(os.contains("win"))
-			{
-				WebDriverManager.chromedriver().setup();
-				driver = new ChromeDriver();
-				LoggingPreferences logPrefs = new LoggingPreferences();
-				logPrefs.enable(LogType.BROWSER, Level.SEVERE);
-				System.out.println("Using Google Chrome");
-				driver.get("https://www.google.com/");
-				System.out.println(os);
+	public static WebDriver setDriver()
+	{
+		Properties prop = Configutil.configdatafile("Config.properties");
+		String os = System.getProperty("os.name").toLowerCase();
+
+		try {
+			if (driver == null) {
+				if (os.contains("windows"))
+				{
+					WebDriverManager.chromedriver().setup();
+					driver = new ChromeDriver();
+					LoggingPreferences logPrefs = new LoggingPreferences();
+					logPrefs.enable(LogType.BROWSER, Level.SEVERE);
+				}
+				else
+				{
+					System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+					String downloadFilepath = System.getProperty("downloadFilepath");
+					HashMap<String, Object> chromePrefs = new HashMap<>();
+					chromePrefs.put("profile.default_content_settings.popups", 0);
+					chromePrefs.put("download.default_directory", downloadFilepath);
+
+
+					ChromeOptions options = new ChromeOptions();
+					options.setExperimentalOption("prefs", chromePrefs);
+					options.addArguments("--no-sandbox");
+					options.addArguments("--headless"); //should be enabled for Jenkins
+					options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
+					options.addArguments("--window-size=1920x1080"); //should be enabled for Jenkins
+
+				}
 			}
-			else {
-
-				System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-				String downloadFilepath = System.getProperty("downloadFilepath");
-				HashMap<String, Object> chromePrefs = new HashMap<>();
-				chromePrefs.put("profile.default_content_settings.popups", 0);
-				chromePrefs.put("download.default_directory", downloadFilepath);
-//				WebDriverManager.chromedriver().setup();
-				ChromeOptions options = new ChromeOptions();
-				options.setExperimentalOption("prefs", chromePrefs);
-				options.addArguments("--no-sandbox");
-				options.addArguments("--headless"); //should be enabled for Jenkins
-				options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
-				options.addArguments("--window-size=1920x1080"); //should be enabled for Jenkins
-
-				driver = new ChromeDriver(options);
-
-				LoggingPreferences logPrefs = new LoggingPreferences();
-				logPrefs.enable(LogType.BROWSER, Level.SEVERE);
-				System.out.println("Using Google Chrome");
-				driver.get("https://www.google.com/");
-				System.out.println(os);
-			}
-
+			driver.get(baseurl);
+		}catch (Exception e) {
+			e.getMessage();
 		}
 
 		return driver;
 	}
+
+			/*
+				if (BrowserX.equalsIgnoreCase("Chrome")) {
+					System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
+					driver = new ChromeDriver();
+					LoggingPreferences logPrefs = new LoggingPreferences();
+					logPrefs.enable(LogType.BROWSER, Level.SEVERE);
+					System.out.println("Using Google Chrome");
+
+				}
+
+				if (BrowserX.equalsIgnoreCase("Chrome")) {
+					WebDriverManager.chromedriver().setup();
+					driver = new ChromeDriver();
+					LoggingPreferences logPrefs = new LoggingPreferences();
+					logPrefs.enable(LogType.BROWSER, Level.SEVERE);
+					System.out.println("Using Google Chrome");
+				}
+
+				else if (BrowserX.equalsIgnoreCase("Firefox")) {
+					WebDriverManager.firefoxdriver().setup();
+					driver = new FirefoxDriver();
+					LoggingPreferences logPrefs = new LoggingPreferences();
+					logPrefs.enable(LogType.BROWSER, Level.SEVERE);
+					System.out.println("Using Google Chrome");
+				} else if (BrowserX.equalsIgnoreCase("Edge")) {
+					WebDriverManager.edgedriver().setup();
+					driver = new EdgeDriver();
+					LoggingPreferences logPrefs = new LoggingPreferences();
+					logPrefs.enable(LogType.BROWSER, Level.SEVERE);
+					System.out.println("Using Microsoft Edge");
+				}
+				else if (BrowserX.equalsIgnoreCase("IE")) {
+					WebDriverManager.iedriver().setup();
+					driver = new InternetExplorerDriver();
+					LoggingPreferences logPrefs = new LoggingPreferences();
+					logPrefs.enable(LogType.BROWSER, Level.SEVERE);
+					System.out.println("Using Internet Explorer");
+				}
+
+				driver.manage().window().maximize();
+				driver.get(prop.getProperty("ENVIRONMENTS_url")); */
+
+
+
+	public void analyzeLog() {
+		Properties prop = Configutil.configdatafile("log4j.properties");
+		PropertyConfigurator.configure(prop);
+		LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+		for (LogEntry entry : logEntries) {
+			System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
+
+		}
+	}
+
+
 
 }
